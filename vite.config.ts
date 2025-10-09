6e058e377384d0cdb97518f90aeb7d837101bd4e
andre luis suckow <andresk.developer@gmail.com>
2025-10-09 14:28:20 -0300

    mais uma uma alteraçãozinha

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({ 
+export default defineConfig(({ mode }) => ({
+  base: '/fuckner-scale-up', 
   server: {
-    base: '/fuckner-scale-up',
     host: "::",
     port: 8080,
   },
