96e2099dc11b27ef472408bd6b50f79022bf7444
André Suckow <andresk.developer@gmail.com>
2026-02-19 18:44:34 -0300

    nhanahnahna

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/fuckner-scale-up">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
